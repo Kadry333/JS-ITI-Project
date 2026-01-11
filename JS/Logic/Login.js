142e07c05791ee1519384d58c19cade635112678
Ahmed Tarek <ahmed.tarek.abdulalim.taha@gmail.com>
2026-01-11 20:04:56 +0200

    fix bug in login

--- JS/Logic/Login.js
+++ JS/Logic/Login.js
@@ -1,41 +1,35 @@
-
 // sessionStorage.clear();
 // localStorage.clear();
-document.getElementById("loginForm").addEventListener("submit", async function (e) {
-    e.preventDefault();
-    let userName = document.getElementById("username").value;
-    let password = document.getElementById("password").value;
-    let currentUser = await UserService.login(userName, password);
-    let loginMessage = document.getElementById("loginMessage");
-    if (!currentUser) {
-        loginMessage.textContent = "Invalid username or password";
-        loginMessage.classList.remove("hidden");
-        loginMessage.classList.remove("text-red-700");
-        loginMessage.classList.add("text-green-700");
-        return;
-    }
+document.getElementById('loginForm').addEventListener('submit', async function (e) {
+  e.preventDefault();
+  let userName = document.getElementById('username').value;
+  let password = document.getElementById('password').value;
+  let currentUser = await UserService.login(userName, password);
+  let loginMessage = document.getElementById('loginMessage');
+  if (!currentUser) {
+    loginMessage.textContent = 'Invalid username or password';
+    loginMessage.classList.remove('hidden');
+    loginMessage.classList.remove('text-green-500', 'text-green-700');
+    loginMessage.classList.add('text-red-500', 'text-red-700');
+    loginMessage.style.color = '#ef4444';
+    return;
+  }
 
-    loginMessage.textContent = `Welcome Back ${currentUser.username}`;
-    loginMessage.className = "mt-4 rounded-md bg-green-500/10 px-4 py-3 text-center text-green-300 outline outline-1 outline-red-500/30 hidden";
-    loginMessage.style.color = "greenyellow";
-    loginMessage.classList.remove("hidden");
-    loginMessage.classList.remove("text-red-700");
-    loginMessage.classList.add("text-green-500");
-    if (localStorage.getItem("userId") == null) {
-        sessionStorage.setItem("currentUser", JSON.stringify(currentUser));
-        localStorage.setItem("userId", currentUser.id);
-        setTimeout(() => {
-            if (currentUser.role == "student")
-                window.location.href = "Student_Dashboard.html";
-            else
-                window.location.href = "Teacher_Exam.html";
-        }, 1000);
-    }
-    else
-    {
-        window.location.href = "login.html";
-    }
+  loginMessage.textContent = `Welcome Back ${currentUser.username}`;
+  loginMessage.className =
+    'mt-4 rounded-md bg-green-500/10 px-4 py-3 text-center text-green-300 outline outline-1 outline-red-500/30 hidden';
+  loginMessage.style.color = 'greenyellow';
+  loginMessage.classList.remove('hidden');
+  loginMessage.classList.remove('text-red-700');
+  loginMessage.classList.add('text-green-500');
 
+  // Set user session
+  sessionStorage.setItem('currentUser', JSON.stringify(currentUser));
+  localStorage.setItem('userId', currentUser.id);
 
-
+  // Redirect based on role
+  setTimeout(() => {
+    if (currentUser.role == 'student') window.location.href = 'Student_Dashboard.html';
+    else window.location.href = 'Teacher_Exam.html';
+  }, 1000);
 });
